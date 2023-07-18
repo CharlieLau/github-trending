109ac499a3f86811626e212e7af8a8eba9a2a71b
liuqingqing05 <liuqingqing05@meituan.com>
2023-07-18 17:53:03 +0800

    feat: 支持生成md

---
+++ scrape.ts
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import * as fs from 'fs';
+import * as cheerio from 'cheerio';
+import * as child_process from 'child_process';
+
+function gitAddCommitPush(date: string, filename: string) {
+  const cmdGitAdd = `git add ${filename}`;
+  const cmdGitCommit = `git commit -m "${date}"`;
+  const cmdGitPush = 'git push -u origin master';
+
+  child_process.execSync(cmdGitAdd);
+  child_process.execSync(cmdGitCommit);
+  child_process.execSync(cmdGitPush);
+}
+
+function createMarkdown(date: string, filename: string) {
+  fs.writeFileSync(filename, `## ${date}\n`);
+}
+
+async function scrape(language: string, filename: string) {
+  const headers = {
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip,deflate,sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+  };
+
+  const url = `https://github.com/trending/${language}`;
+  const response = await axios.get(url, { headers });
+  const $ = cheerio.load(response.data);
+  const items = $('div.Box article.Box-row');
+
+  fs.appendFileSync(filename, `\n#### ${language}\n`, 'utf-8');
+
+  items.each((_, item) => {
+    const title = $(item).find('.lh-condensed a').text().replace(/\s+/g, '').replace(/\n+/g,'').trim();
+    const owner = $(item).find('.lh-condensed span.text-normal').text().replace(/\s+/g, '').replace(/\n+/g,'').trim();
+    const description = $(item).find('p.col-9').text().replace(/\s+/g, '').replace(/\n+/g,'').trim();
+    console.log(owner)
+    const url = `https://github.com${$(item).find('.lh-condensed a').attr('href')}`;
+
+    fs.appendFileSync(
+      filename,
+      `* [${title}](${url}):${description}\n`,
+      'utf-8'
+    );
+  });
+}
+
+async function job() {
+  const strDate = new Date().toISOString().slice(0, 10);
+  const filename = `${strDate}.md`;
+
+  createMarkdown(strDate, filename);
+
+  await scrape('javascript', filename);
+  await scrape('html', filename);
+  await scrape('css', filename);
+  await scrape('python', filename);
+
+  gitAddCommitPush(strDate, filename);
+}
+
+job();
